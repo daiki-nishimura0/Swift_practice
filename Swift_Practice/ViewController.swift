56cc377d6d5c66185b03a25f0b072c3d3d95bc66
daiki nishimura0 <nishimura_dai@deltanet.co.jp>
2021-09-17 11:27:30 +0900

    Initial Commit

---
+++ Swift_Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift_Practice
+//
+//  Created by daiki nishimura on 2021/09/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
