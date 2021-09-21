0d669d122d0d3f3206b046de39687caf5f0cd272
daiki nishimura0 <nishimura_dai@deltanet.co.jp>
2021-09-21 09:47:16 +0900

    for-in文

--- Swift_Practice/ViewController.swift
+++ Swift_Practice/ViewController.swift
@@ -165,6 +165,16 @@ class ViewController: UIViewController {
 //        } while n > 1
 //        print(n)
         
+        // for-in文
+        for i in 1 ..< 10 {
+            var line = ""
+            for j in 1 ..< 10 {
+                let n = i * j
+                if n < 10 { line += " "}
+                line += "\(n)"
+                }
+            print(line)
+        }
         
         
         super.viewDidLoad()
