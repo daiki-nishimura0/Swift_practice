c5888b0984ee488d2b716fe714badfac47095f7a
daiki nishimura0 <nishimura_dai@deltanet.co.jp>
2021-09-22 09:17:35 +0900

    オーバーロード

--- Swift_Practice/ViewController.swift
+++ Swift_Practice/ViewController.swift
@@ -11,6 +11,21 @@ import UIKit //フレームワークのインポート
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        // オーバーロード 同じ関数名で引数の型や個数が異なる関数の定義
+        func mySwap(_ a: inout Int, _ b: inout Int, _ c: inout Int) {
+            let t = a; a = b; b = c; c = t
+        }
+        
+        var s = 10, t = 20
+        var x = 1, y = 2, z = 3
+        mySwap(&s, &t)
+        print("s=\(s), t=\(t)")
+        mySwap(&x, &y, &z)
+        print("x=\(x), y=\(y), z=\(z)")
+        
+        func mySwap(_ a: inout Int, _ b: inout Int) {
+            let t = a; a = b; b = t
+        }
         
 //        // 関数内の関数
 //        func printMonth(first fday:Int, days:Int) {
