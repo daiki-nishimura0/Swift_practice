fecdad9c9cd804edfd2f7f9071f63769c265c2b2
daiki nishimura0 <nishimura_dai@deltanet.co.jp>
2021-09-24 13:26:54 +0900

    イニシャライザとメソッド

--- Swift_Practice/ViewController.swift
+++ Swift_Practice/ViewController.swift
@@ -11,6 +11,24 @@ import UIKit //フレームワークのインポート
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+  
+        // イニシャライザとメソッド
+        struct DateWithString {
+            let string: String
+            let year, mouth, day:Int
+            init(_ y:Int, _ m: Int, _ d:Int) {
+                year = y; mouth = m; day = d;
+                string = "\(y)-" + DateWithString.twoDigits(m)
+                    + "-" + DateWithString.twoDigits(d)
+            }
+            static func twoDigits(_ n:Int) -> String {
+                let i = n % 100
+                return i < 10 ? "0\(i)" : "\(i)"
+            }
+        }
+        let an1 = DateWithString(2015, 6, 22)
+        print(an1.string)
+        
         
 //        // タイプメソッド
 //        struct SimpleDate {
