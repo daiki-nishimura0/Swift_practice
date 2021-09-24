315d3571caea329c6f4587649375a7032070d37d
daiki nishimura0 <nishimura_dai@deltanet.co.jp>
2021-09-24 14:09:22 +0900

    構造体内部での演算子の定義

--- Swift_Practice/ViewController.swift
+++ Swift_Practice/ViewController.swift
@@ -12,6 +12,42 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
   
+        
+//        // 構造体の内部での演算子の定義
+//        struct Clock {
+//            var hour = 0, min = 0
+//            mutating func advance(min:Int) {
+//                let m = self.min + min
+//                if m >= 60 {
+//                    self.min = m % 60
+//                    let t = self.hour + m / 60
+//                    self.hour = t % 24
+//                }else{
+//                    self.min = m
+//                }
+//            }
+//                func toString() -> String {
+//                    let h = hour < 10 ? "\(hour)":"\(hour)"
+//                    let m = min < 10 ? "0\(min)":"\(min)"
+//                    return h + ":" + m
+//                }
+//            
+//                static func +(lhs: Clock, rhs: Int) -> Clock {
+//                    var t = lhs
+//                    t.advance(min: rhs)
+//                    return t
+//                }
+//                static func +=(lhs: inout Clock, rhs: Int) {
+//                    lhs.advance(min: rhs)
+//                }
+//            }
+//        let tic = Clock(hour:19, min:40)
+//        var tac = tic + 75
+//        print(tac.toString())
+//        tac += 125
+//        print(tac.toString())
+//    
+
 //        // イニシャライザとメソッド
 //        struct DateWithString {
 //            let string: String
